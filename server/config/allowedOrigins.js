ce3a7e6ccf1d15b34395cef18a33de864a6c553e
michaltruszczynski <michal.truszczynski2@gmail.com>
2023-01-28 14:59:25 +0100

    changed env var

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
         "http://localhost:3500",
         "http://localhost:3000",
+        "https://kshopapi.onrender.com"
      ];
      
 module.exports = allowedOrigins;
