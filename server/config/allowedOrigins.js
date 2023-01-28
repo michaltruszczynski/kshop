d7dd10c1ccdbed3572e4f2af9522fe6f837f1187
michaltruszczynski <michal.truszczynski2@gmail.com>
2023-01-28 15:08:37 +0100

    fixed api cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
         "http://localhost:3500",
         "http://localhost:3000",
-        "https://kshopapi.onrender.com"
+        "https://eshop-54sk.onrender.com"
      ];
      
 module.exports = allowedOrigins;
